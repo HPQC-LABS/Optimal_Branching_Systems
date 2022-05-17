099443c9995efb42cbf3ff22f2f8b78697a8924e
Nike Dattani <dattani.nike@gmail.com>
2022-05-17 14:58:47 -0400

    Create weightedMatroidIntersection.m

---
+++ weightedMatroidIntersection.m
@@ -0,0 +1,11 @@
+% Examples matroids: M1=(E,F1) and M2=(E,F2) where E={1,2,3},F1={0,{1},{2},{1,2}),F2={0,{1},{2},{3}}
+
+N=3;         % number of elements in E.
+K=10;        % number of iterations k.
+c=rand(N,1); % randomly assign real-number weights (c) to the elements of E.
+k=0;X=0;c1=zeros(N,1);c2=c1; 
+for y=1:(N - nnz(X))
+    for i=1:2
+        C(i,y)=
+    end
+end
