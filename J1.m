0f1b48829505ca5cb501d3308e8226545808f5f3
Nike Dattani <dattani.nike@gmail.com>
2022-09-03 19:16:34 -0400

    Same as previous commit but for a different line

--- J1.m
+++ J1.m
@@ -10,3 +10,4 @@ Are_more_than_k_edges_entering_a_nonRoot = sum(sum(G_incoming_to_nonRoots,2)>nnz
 % Are_more_than_k_edges_entering_a_nonRoot = sum(sum((1-roots)'.*(G>0),2)>nnz(roots))     % non-0 if there's more than k edges entering any non-root, 0 otherwise. [more comapct]
   
 are_we_in_J1 = (Are_any_edges_entering_a_root+Are_more_than_k_edges_entering_a_nonRoot)<1 % 1 if the input is in J1, 0 otherwise.
+% are_we_in_J1 = (sum(sum(roots'.*(G>0)))>0 + sum(sum((1-roots)'.*(G>0),2)>nnz(roots)))<1 % 1 if the input is in J1, 0 otherwise. [more compact]
