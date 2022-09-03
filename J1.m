b1333c9ea40ce9a3cf178f5c0021e6af9add86d6
Nike Dattani <dattani.nike@gmail.com>
2022-09-03 19:14:40 -0400

    This for J1.m:
    - Added more compact version of some of the J1.m lines as comments.
    - corrected some erroneous existing comments on the other lines

--- J1.m
+++ J1.m
@@ -2,9 +2,11 @@ function are_we_in_J1 = J1(G,roots)                                             
 
 G_incoming = G>0                                                                          % Remove all outgoing edges from G
 G_incoming_to_roots = roots'.*G_incoming                                                  % Remove all edges not entering a root
-Are_any_edges_entering_a_root=sum(sum(G_incoming_to_roots))>0                             % 1 if there's edges entering a root, 0 otherwise.
+Are_any_edges_entering_a_root=sum(sum(G_incoming_to_roots))>0                             % non-0 if there's edges entering a root, 0 otherwise.
+% Are_any_edges_entering_a_root = sum(sum(roots'.*(G>0)))>0                               % non-0 if there's edges entering a root, 0 otherwise. [more compact]
   
 G_incoming_to_nonRoots  = (1-roots)'.*G_incoming                                          % Remove all edges not entering a non-root
-Are_more_than_k_edges_entering_a_nonRoot = sum(sum(G_incoming_to_nonRoots,2)>nnz(roots))  % 1 if there's more than k edges entering any non-root, 0 otherwise.
+Are_more_than_k_edges_entering_a_nonRoot = sum(sum(G_incoming_to_nonRoots,2)>nnz(roots))  % non-0 if there's more than k edges entering any non-root, 0 otherwise.
+% Are_more_than_k_edges_entering_a_nonRoot = sum(sum((1-roots)'.*(G>0),2)>nnz(roots))     % non-0 if there's more than k edges entering any non-root, 0 otherwise. [more comapct]
   
 are_we_in_J1 = (Are_any_edges_entering_a_root+Are_more_than_k_edges_entering_a_nonRoot)<1 % 1 if the input is in J1, 0 otherwise.
