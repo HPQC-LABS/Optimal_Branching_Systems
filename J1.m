8aad9818aeedac3a5cb90c59bfabf7edcbc8c2bf
Nike Dattani <dattani.nike@gmail.com>
2022-09-03 19:27:03 -0400

    Update J1.m

--- J1.m
+++ J1.m
@@ -1,4 +1,4 @@
-function are_we_in_J1 = J1(G,roots)                                                       % G is the incidence matrix, "roots" tells us which vertices are roots.
+function are_we_in_J1 = J1(G,roots)                                                       % G is the incidence matrix of a real-weighted digraph (positive: edge is entering the vertex, negative: edge is exiting), "roots" tells us which vertices are roots.
 
 G_incoming = G>0                                                                          % Remove all outgoing edges from G
 G_incoming_to_roots = roots'.*G_incoming                                                  % Remove all edges not entering a root
@@ -11,3 +11,5 @@ Are_more_than_k_edges_entering_a_nonRoot = sum(sum(G_incoming_to_nonRoots,2)>nnz
   
 are_we_in_J1 = (Are_any_edges_entering_a_root+Are_more_than_k_edges_entering_a_nonRoot)<1 % 1 if the input is in J1, 0 otherwise.
 % are_we_in_J1 = (sum(sum(roots'.*(G>0)))>0 + sum(sum((1-roots)'.*(G>0),2)>nnz(roots)))<1 % 1 if the input is in J1, 0 otherwise. [more compact]
+
+% roots=[1 0 1 0]; G = rand(4,6).*(-1).^randi([0,1],4,6)                                  % A simple example on which you can test the above code!!!
